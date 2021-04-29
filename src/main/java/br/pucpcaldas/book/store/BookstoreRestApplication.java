70f77b0af9ddbb118bf3959ee0b9b13a1a0aa1b5
Marcelo Assis <marceloassis50@gmail.com>
2021-04-29 20:38:17 -0300

    first commit

---
+++ src/main/java/br/pucpcaldas/book/store/BookstoreRestApplication.java
@@ -0,0 +1,11 @@
+package br.pucpcaldas.book.store;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class BookstoreRestApplication extends Application {
+}
